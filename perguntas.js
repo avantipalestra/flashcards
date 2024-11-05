7c06e688ba0e44bdff107c83fe6aa3966228b935
avantipalestra <00001078691010sp@al.educacao.sp.gov.br>
2024-11-05 15:30:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,93 @@
+criaCartao(
+    'Língua Portuguesa',
+    'Linguagem formal: Definição?',
+    'É aquela que carrega consigo a rigidez das normas gramaticais, utilizada principalmente em textos e profissões que a exigem, como no Direito, Letras e História.'
+)
+
+criaCartao(
+    'Geografia',
+    'Cartografia',
+    'Ciência que estuda a representação de uma área geográfica ou uma superfície plana.'
+)
+
+criaCartao(
+    'Biologia',
+    'Genes dominantes: Como são convencionalmente representados?',
+    'Por letras maiúsculas.'
+)
+
+criaCartao(
+    'Matemática',
+    'Fatorial: Como é calculado?',
+    'O fatorial de um número é calculado pela multiplicação desse número por todos os seus antecessores até chegar ao número 1.'
+)
+criaCartao(
+    'Químnica',
+    'Podem ser de três tipos. São elas…',
+    'Ligação iônica;Ligação covalente;Ligação metálica.'
+)
+
+criaCartao(
+    'Física',
+    'Eletromagnetismo O que estuda?',
+ 'Relação entre forças da eletricidade e magnetismo como fenômeno único'
+    
+)
+
+criaCartao(
+    'Filosofia',
+    'O que foi o Iluminismo?',
+    'Movimento intelectual e filosófico, que tinha a “razão” como a principal fonte de autoridade e legitimidade.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'História',
+    'Era Vargas: Como se divide?',
+    'Governo provisório;Governo constitucional;Estado novo.'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu "Dom Quixote"?',
+    'Miguel de Cervantes'
+)
+
+criaCartao(
+    'Fisíca',
+    'Qual a terceira lei de Newton?',
+    'Para cada ação, há uma reação igual e oposta'
+)
+
+criaCartao(
+    'Química',
+    'Qual o elemento simbolo "O" ?',
+    'Oxigênio'
+)
+criaCartao(
+    'Qual é a molécula responsável pela transmissão de características hereditárias?',
+    'O que é Python?',
+    'DNA (ácido desoxirribonucleico)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o rio mais longo do mundo?',
+    'Rio Nilo'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o principal motivo da Revolução francesa?',
+    'A insatisfação com a monarquia absoluta e as desigualdades sociais e econômicas'
+)
